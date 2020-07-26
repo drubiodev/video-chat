25a87968550d8e3ffb27055dde36adc11c080036
Daniel Rubio <drubio432@gmail.com>
2020-07-25 22:25:51 -0400

    added route for rooms

--- server.js
+++ server.js
@@ -4,4 +4,21 @@ const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const PORT = 3000;
 
+const {
+    v4: uuidV4
+} = require('uuid');
+
+app.set('view engine', 'ejs');
+app.set(express.static('public'));
+
+//Routes
+app.get('/', (req, res) => {
+    res.redirect(`/${uuidV4()}`)
+})
+app.get('/:room', (req, res) => {
+    res.render('room', {
+        roomId: req.params.room
+    })
+})
+
 server.listen(PORT);
