6d113d763ef51886d8ba0d2c66a5e309ca202c2d
Daniel Rubio <drubio432@gmail.com>
2020-07-27 19:24:21 -0400

    add script for peerjs

--- public/script.js
+++ public/script.js
@@ -1,6 +1,13 @@
 const socket = io('/');
+const peer = new Peer(undefined, {
+    host: '/',
+    port: '3001'
+});
 
-socket.emit('join-room', ROOM_ID, 10);
+peer.on('open', (id) => {
+    socket.emit('join-room', ROOM_ID, id);
+});
+
 socket.on('user-connected', userId => {
     console.log(`User Connected: ${userId}`);
 });
